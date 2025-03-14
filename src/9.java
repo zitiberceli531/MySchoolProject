f1351278dbde01291c2aa535ac719d00927cc354
zitiberceli531 <zitiberceli531@gmail.com>
2025-03-14 15:57:58 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MySchoolProject{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
+}
