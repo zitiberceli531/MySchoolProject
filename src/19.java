fb8c6501292a15fec494f35d7a3dd2f71ce4cd4f
zitiberceli531 <zitiberceli531@gmail.com>
2025-03-30 08:23:49 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,7 @@
+public class StudentManagementSystem {
+    // Example of student information structure
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
