89072a7284cb8b8caac97e21ba42cddd91c104da
zitiberceli531 <zitiberceli531@gmail.com>
2025-04-08 08:01:06 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,6 @@
+public class StudentProgram {
+    public static void main(String[] args) {
+        // Example of simple Java program that prints "Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
