5ab8421815a61206df07cb1ceab83e3f4caaa8a8
zitiberceli531 <zitiberceli531@gmail.com>
2025-04-23 11:07:05 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,15 @@
+public class MyCode {
+    public static void main(String[] args) {
+        // Example of student's name
+        String name = "Alice";
+        
+        // Example of student's age
+        int age = 15;
+        
+        // Example of student's grade level
+        String grade = "9th Grade";
+        
+        // Example of student's favorite subject
+        String favoriteSubject = "Math";
+    }
+}
