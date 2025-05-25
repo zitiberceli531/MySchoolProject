fa0e01a04a08ba44cfe4dd563d59343438a9929a
zitiberceli531 <zitiberceli531@gmail.com>
2025-05-25 07:52:08 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,16 @@
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Code to initialize your project and set up initial variables
+        System.out.println("Hello, world!");
+        int x = 10;
+        double y = 2.5;
+        String name = "Alice";
+        boolean isOnline = true;
+
+        // Display the values of the variables
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("name: " + name);
+        System.out.println("isOnline: " + isOnline);
+    }
+}
